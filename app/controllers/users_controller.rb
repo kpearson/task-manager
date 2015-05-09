766a7684a29f6877db77c8f3aed63b3428d6379e
Kit <dev_kit@icloud.com>
2015-05-09 15:55:53 -0600

    add users model, controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       redirect_to root_path, notice: "Welcome #{user.name}"
     else
-      redirect_to :back
+      redirect_to :back, notice: "Please enter valid information"
     end
   end
 
--- spec/features/user/login_spec.rb
+++ spec/features/user/login_spec.rb
@@ -1,23 +1,13 @@
 require "rails_helper"
 
-describe "User" do
-  it "can sign up" do
+describe "Returning user" do
+  it "can login" do
+    create(:user)
     visit root_path
-    click_link_or_button "Please Sign Up"
-    fill_in "user[name]",     with: "Kit"
-    fill_in "user[email]",    with: "kit@kit.com"
-    fill_in "user[password]", with: "password"
-    click_button "Create User"
-    expect(page).to have_content("Welcome Kit")
-  end
-
-  xit "can login" do
-    user = create!(:user)
-
-    visit_root_path
-    click_link_or_button "Log in"
-    fill_in "user[name]",     with: "Kit"
+    click_link_or_button "Sign In"
+    fill_in "user[email]",    with: "kit@example.com"
     fill_in "user[password]", with: "password"
     click_button "Submit"
+    expect(page).to have_content "Welcome back Kit!"
   end
 end
