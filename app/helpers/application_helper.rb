fee363e7a88a30b55279f924bba2120a5fea8601
Kit <dev_kit@icloud.com>
2015-05-09 16:12:19 -0600

    modify flash messages with fade out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,18 @@
 module ApplicationHelper
 
   def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
+    case flash_type
+    when :success
+      "alert-success"
+    when :danger
+      "alert-danger"
+    when :warning
+      "alert-warning"
+    when :info
+      "alert-info"
+    else
+      flash_type.to_s
+    end
   end
 
-  def flash_messages(opts = {})
-    flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
-        concat message
-      end)
-    end
-    nil
-  end
 end
