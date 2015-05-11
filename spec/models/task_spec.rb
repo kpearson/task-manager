1f40f90fd99f1b0f4f0a6c2c53d2a624c72ce02f
Kit <dev_kit@icloud.com>
2015-05-11 03:00:50 -0600

    add tasks

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Task" do
+  it "belongs to a list" do
+    expect(Task.create(title: "Coding task",
+                       description: "Build a task manager")
+          ).not_to be_valid
+
+    list = create(:list)
+    expect(Task.create(title: "Coding task",
+                       descripton: "Build a task manager",
+                       list: list)
+          ).to be_valid
+  end
+end
